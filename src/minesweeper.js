5f3b1b253d053a051cb1c21b55dce45ee6758748
Catalina-Tudor <catalina.tudor@fundingcircle.com>
2019-01-25 15:58:05 +0000

    Automate board generation

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,51 @@
-const printBoard = (board) => {
-    console.log('Current board: ');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+	const board = [];
+
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		const row = [];
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(' ');
+		}
+		board.push(row);
+	};
+	return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		let row = [];
+
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(null);
+		}
+
+		board.push(row);
+	};
+
+	let numbersOfBombsPlaced = 0;
+
+	while (numbersOfBombsPlaced < numberOfBombs) {
+		// this might place bombs on top of already existing ones;
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex =  Math.floor(Math.random() * numberOfColumns);
+
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numbersOfBombsPlaced++;
+	}
+
+	return board;
 };
 
+const printBoard = board => {
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-let board = [
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' '],
-];
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
